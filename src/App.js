26c2479d18a919e9b51236f1be43197b4132fb69
Artur <sarnackiartur@gmail.com>
2022-02-18 17:02:19 +0100

    added some ui and logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import HomeView from './components/HomeView/HomeView';
+import "./App.css";
+import HomeView from "./components/HomeView/HomeView";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HomeView></HomeView>
+    <div className="p-14">
+      <Link to='/' className="flex justify-center text-4xl text-yellow-700">Pokemon picker</Link>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
+      {/* <HomeView></HomeView> */}
     </div>
   );
 }
