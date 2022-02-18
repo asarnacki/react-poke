8f36c6a2c92c060eea96e98667137da2af852263
Artur <sarnackiartur@gmail.com>
2022-02-18 17:01:57 +0100

    added some ui and logic

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,16 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './HomeView.css';
+import React from "react";
 
-const HomeView = () => (
-  <div className="w-full flex justify-center">
-    Pokemon Picker
-    <input type='text' placeholder='Enter pokemon here...' className='mt-10'></input>
-  </div>
-);
+class HomeView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      uldIdLookUp: {},
+      text: "",
+      // filteredPoke: this.updatePoke(),
+    };
+  }
 
-HomeView.propTypes = {};
+  // updatePoke() {
+  //   if (!this.state.text) {
+  //     return [];
+  //   }
+  //   return this.state.pokemons.filter((poke) =>
+  //     poke.name.includes(this.state.text)
+  //   );
+  // }
 
-HomeView.defaultProps = {};
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.state.pokemons = data.results;
+      });
+  }
+
+  render() {
+    return (
+      <div className="w-full flex justify-center">
+        <input
+          placeholder="Enter pokemon here..."
+          className="mt-10 p-2 border-blue-500 border-2"
+        ></input>
+        {this.state.pokemons.map((val, index) => {
+          return <li key={index}>{val}</li>;
+        })}
+      </div>
+    );
+  }
+}
+// const API = "https://pokeapi.co/api/v2/pokemon?offset=0";
+// const HomeView = () => (
+//   <div className="w-full flex justify-center">
+//     <input
+//       type="text"
+//       placeholder="Enter pokemon here..."
+//       className="mt-10 p-2 border-blue-500 border-2"
+//     ></input>
+//   </div>
+// );
+
+// fetch(API)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// HomeView.propTypes = {};
+
+// HomeView.defaultProps = {};
 
 export default HomeView;
