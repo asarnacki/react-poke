6b11713c7fd9fb579820af2e8913df9f7e7dbcbc
Artur <sarnackiartur@gmail.com>
2022-03-21 09:45:17 +0100

    addee filtering and gh-pages

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -13,6 +13,7 @@ export interface IPokemon {
 
 const HomeView: FunctionComponent<IPokemon> = () => {
   const [pokemon, setPokemon] = useState<IPokemon[]>([]);
+  const [query, setQuery] = useState("");
   // const API = "https://pokeapi.co/api/v2/pokemon?offset=0";
 
   const getPokemons = useCallback(async () => {
@@ -29,25 +30,38 @@ const HomeView: FunctionComponent<IPokemon> = () => {
   useEffect(() => {
     getPokemons();
   }, [getPokemons]);
+
   return (
     <>
       <div className="w-full flex justify-center">
         <input
           placeholder="Enter pokemon here..."
+          onChange={(event) => setQuery(event.target.value)}
           className="mt-10 p-2 border-blue-500 border-2"
         ></input>
       </div>
       <div className="mt-10 p4 flex flex-wrap justify-center">
         <div className="ml-4 text-2px text-blue-400">
-          {pokemon.map((item, idx) => (
-            <Link key={idx}
-              to={{
-                pathname: `/about/${idx}`,
-              }}
-            > 
-              {item.name}{" "}
-            </Link>
-          ))}
+          {pokemon
+            .filter((post) => {
+              if (query === "") {
+                return post;
+              } else if (
+                post.name.toLowerCase().includes(query.toLocaleLowerCase())
+              ) {
+                return post;
+              }
+            })
+            .map((item, idx) => (
+              <Link
+                key={idx+1}
+                to={{
+                  pathname: `/about/${idx+1}`,
+                }}
+              >
+                {item.name}{" "}
+              </Link>
+            ))}
         </div>
       </div>
     </>
